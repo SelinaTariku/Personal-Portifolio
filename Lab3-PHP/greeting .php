f9a51d3b1ae3e3c750c2f222aacbe531474b3085
Selamawit Tariku <109794317+SelinaTariku@users.noreply.github.com>
2024-03-25 10:07:41 +0300

    first commit

---
+++ Lab3-PHP/greeting .php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello, World!</h1>
+    <?php
+      $name = "Selamawit"; 
+
+    echo "<h1>Hello, $name!</h1>";
+    echo "<p>Hello, World</p>";
+    echo "<h1>Hello, World!</h1>";
+
+    echo "Comment <br>";
+    echo "// single line comment <br>";
+    echo "# single line comment <br>";
+    echo "/* multi line comment */ <br  >";
+
+    ?>
+
+</body>
+
+</html>
